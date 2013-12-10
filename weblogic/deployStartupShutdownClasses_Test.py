7aead9b628b15fe26b5d1e2d78e4ff7089aaeb70
vagrant <vagrant@weblogic1036.(none)>
2013-12-10 18:31:20 +0000

    added setUp and tearDown methods, to create and destroy a temporary domain

--- weblogic/deployStartupShutdownClasses_Test.py
+++ weblogic/deployStartupShutdownClasses_Test.py
@@ -1,9 +1,8 @@
 
 import wl
 import unittest
-import mock
-from my_package import *
-
+import time
+"""
 class TestApplyStartupClass(unittest.TestCase):
 	def setUp(self):
 		raise 'not implemented'
@@ -17,10 +16,28 @@ class TestApplyStartupClass(unittest.TestCase):
 		raise 'not implemented'
 	def tearDown(self):
 		raise 'not implemented'
+"""
 
+class TestStartupClassComparable(unittest.TestCase):
+	def setUp(self):
+		self.domainDir='/tmp/'+repr(time.time())
+		templateFile='/home/vagrant/Oracle/Middleware/wlserver_10.3/common/templates/domains/wls.jar'
+		self.username='weblogic'
+		self.password='aaahL00k@AllDLon3lyPPl'
+		print "Creating domain "+self.domainDir
+		wl.createDomain(templateFile,self.domainDir,self.username,self.password)
+		wl.startServer(domainDir=self.domainDir)
 
-	
+	def test_compareEqual(self):
+		raise 'not implemented'
 
+#	def test_compareDifferent(self):
+#	def test_compareOneIsNull(self):
+
+	def tearDown(self):
+		wl.shutdown(force='true')
+		shutil.rmtree(self.domainDir)
+"""
 class TestStartupClassesAbstraction(unittest.TestCase):
 
 	def setUp(self):
@@ -36,6 +53,6 @@ class TestStartupClassesAbstraction(unittest.TestCase):
 	def test_updateStartupClassArgument(self):
 		self.test_domain.add(self.alreadyExistentStartupClass)
 		self.assertTrue
-
+"""
 if __name__ == "__main__" or __name == "main":
 	unittest.main()
