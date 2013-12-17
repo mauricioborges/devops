ba2865291e8adb9ce0900b675984a966a0eed9fa
mauricioborges <mauricioborges@gmail.com>
2013-12-17 15:25:34 +0000

    fixed some tests after changing the connection method

--- weblogic/deploy_test.py
+++ weblogic/deploy_test.py
@@ -20,23 +20,31 @@ class deployConnections_Test(unittest.TestCase):
     try:
       Deployer().connect,(None,None,None)
     except e:
-      self.assertEqual(deploytools.WrongParameterException,e.type)
+      self.assertEqual(deploytools.WrongParameterExceptionException.__name__,e.__class__.__name__)
   def test_connection_valid(self):
     deployer=Deployer()
     deployer.connect('t3://localhost:7001','user.config','user.key')
     self.assertEqual(deployer.isConnected(),True)
   def test_connection_invalid_url(self):
-    deployer=Deployer()
-    deployer.connect('t3://someserveranywhere:7002','user.config','user.key')
-    self.assertEqual(deployer.isConnected(),False)
+    try:
+      deployer=Deployer()
+      deployer.connect('t3://someserveranywhere:7002','user.config','user.key')
+    except Exception, e:
+      self.assertEqual(deployer.isConnected(),False)
+      self.assertEqual(deploytools.FailedConnectionException.__name__,e.__class__.__name__)
   def test_connection_invalid_configFile(self):
-    deployer=Deployer()
-    deployer.connect('t3://localhost:7001','user.config.invalid','user.key')
-    self.assertEqual(deployer.isConnected(),False)
+    try:
+      deployer=Deployer()
+      deployer.connect('t3://localhost:7001','user.config.invalid','user.key')
+    except Exception, exception:
+      self.assertEqual(deploytools.WrongParameterException.__name__,exception.__class__.__name__)
+
   def test_connection_invalid_keyFile(self):
-    deployer=Deployer()
-    deployer.connect('t3://localhost:7001','user.config','user.key.invalid')
-    self.assertEqual(deployer.isConnected(),False)
+    try:
+      deployer=Deployer()
+      deployer.connect('t3://localhost:7001','user.config','user.key.invalid')
+    except Exception, exception:
+      self.assertEqual(deploytools.FailedConnectionException.__name__,exception.__class__.__name__)
 
 class UserConfigAndKeyFile_Test(unittest.TestCase):
   def test_userConfigOkAndUserKeyOk(self):
@@ -46,15 +54,23 @@ class UserConfigAndKeyFile_Test(unittest.TestCase):
     self.assertEqual(deployer._areUserFilesValid(),True)
   def test_userConfigEmptyAndUserKeyOk(self):
     deployer=Deployer()
+    deployer._userConfigFile='user.config.empty'
+    deployer._userKeyFile='user.key'
     self.assertEqual(deployer._areUserFilesValid(),False)
   def test_userConfigWrongAndUserKeyOk(self):
     deployer=Deployer()
+    deployer._userConfigFile='user.config.invalid'
+    deployer._userKeyFile='user.key'
     self.assertEqual(deployer._areUserFilesValid(),False)
   def test_userConfigOkAndUserKeyEmpty(self):
     deployer=Deployer()
+    deployer._userConfigFile='user.config'
+    deployer._userKeyFile='user.key.empty'
     self.assertEqual(deployer._areUserFilesValid(),False)
   def test_userConfigWrongAndUserKeyWrong(self):
     deployer=Deployer()
+    deployer._userConfigFile='user.config.invalid'
+    deployer._userKeyFile='user.key.invalid'
     self.assertEqual(deployer._areUserFilesValid(),False)
 
 
