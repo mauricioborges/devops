cc1a88a15a35928a1d1a8ceda93634cf8489a844
Mauricio Borges <mauricioborges@gmail.com>
2014-01-10 14:43:21 -0200

    added redmine backlogs and pipeline configuration

--- vagrant/redmine/install_redmine.sh
+++ vagrant/redmine/install_redmine.sh
@@ -7,11 +7,11 @@ if [ "$EUID" -ne "0" ]; then
 fi
 
 
-REDMINE_VERSION=redmine-2.4.2-0
-BITNAMI_PACKAGE="bitnami-$REDMINE_VERSION-linux-installer.run"
-install_dir=/opt/$REDMINE_VERSION
-plugins_dir=$install_dir/apps/redmine/htdocs/plugins
-redmine_htdocs_dir=$install_dir/apps/redmine/htdocs
+export REDMINE_VERSION=redmine-2.4.2-0
+export BITNAMI_PACKAGE="bitnami-$REDMINE_VERSION-linux-installer.run"
+export install_dir=/opt/$REDMINE_VERSION
+export plugins_dir=$install_dir/apps/redmine/htdocs/plugins
+export redmine_htdocs_dir=$install_dir/apps/redmine/htdocs
 require_restart=false
 
 echo "Installing Bitnami Package $REDMINE_VERSION"
@@ -26,28 +26,83 @@ else
 fi 
 echo 'Installation will run now... wait a bit ... '
 chmod +x $BITNAMI_PACKAGE
-./$BITNAMI_PACKAGE --mode unattended --installer-language en --prefix $install_dir --base_user_name "Mauricio Borges" --base_mail ms55042@getnet.com.br --base_user admin --base_password getnet123 --redmine_language pt_br --debugtrace redmine_install.log --apache_server_port 9150 --apache_server_ssl_port 9151 --mysql_port 3336 --mysql_password "S@9842a2Y0" --smtp_enable 1 --debuglevel 4 
+./$BITNAMI_PACKAGE --mode unattended --installer-language en --prefix $install_dir --base_user_name "Mauricio Borges" --base_mail ms55042@getnet.com.br --base_user admin --base_password getnet123 --redmine_language pt_br --apache_server_port 9150 --apache_server_ssl_port 9151 --mysql_port 3336 --mysql_password "S@9842a2Y0" --smtp_enable 1 --debuglevel 4 
 
-status=$?
-if [ $status -ne 0 ]; then
-   echo "Instalation failed with error code $status. Exiting ... "
-fi
+#status=$?
+#if [ $status -ne 0 ]; then
+#   echo "Instalation failed with error code $status. Exiting ... "
+#fi
 
 echo 'Removing installation package ... '
 rm ./$BITNAMI_PACKAGE
 
+#http://wiki.bitnami.com/Native_Installers_Quick_Start_Guide#Debian-like_distribution_%28Debian.2c_Ubuntu.2c_etc%29
+#upstart
+#cp $install_dir/ctlscript.sh /etc/init.d/bitnami-redmine
+#update-rc.d -f bitnami-redmine start 80 2 3 4 5 . stop 30 0 1 6 .
 
+# PREPARE FOR PLUGINS
+
+apt-get -y install make unzip mercurial
+cd $install_dir
+./use_redmine
 
 #INSTALL REDMINE_BACKLOGS http://www.redminebacklogs.net/en/installation.html
 
 backlogs_tag=v1.0.6
 echo "Installing redmine backlogs version $backlogs_tag"
+cd $redmine_htdocs_dir
+bundle install --without development test
 
-apt-get install make unzip mercurial
+gem install holidays --version 1.0.3
+gem install holidays
 
-cd $install_dir
+cd $plugins_dir
+git clone git://github.com/backlogs/redmine_backlogs.git
+cd redmine_backlogs
+git checkout v1.0.6
+bundle install 
+
+cd $redmine_htdocs_dir
+RAILS_ENV=production
+export RAILS_ENV
+bundle install --no-deployment
+bundle update nokogiri
+bundle exec bin/rake db:migrate RAILS_ENV=production
+bundle exec bin/rake tmp:cache:clear RAILS_ENV=production
+bundle exec bin/rake tmp:sessions:clear RAILS_ENV=production
+bundle exec bin/rake redmine:backlogs:install story_trackers=Feature,Bug,Support task_tracker=Task 
+
 
 require_restart=true
+
+
+
+$install_dir/ctlscript.sh restart
+
+#INSTALL REDMINE PIPELINE PLUGIN
+cd $plugins_dir
+git clone https://github.com/GitDries/redmine_pipeline_plugin
+
+exit
+
+#INSTALL REDMINE CHARTS 2 
+
+cd $redmine_htdocs_dir
+plugin_charts_dir=plugins/redmine_charts2
+git clone git://github.com/pharmazone/redmine_charts2 $plugin_charts_dir
+cd $plugin_charts_dir
+git checkout redmine21
+
+cd $redmine_htdocs_dir
+git clone git://github.com/pullmonkey/open_flash_chart.git plugins/open_flash_chart
+
+cd $redmine_htdocs_dir
+cp -r plugins/open_flash_chart/assets public/plugin_assets/open_flash_chart
+
+
+cd $redmine_htdocs_dir
+bin/rake redmine:plugins:migrate RAILS_ENV=production
 
 #INSTALL REDMINE PROJECTS SECTIONS
 cd $plugins_dir
@@ -77,8 +132,8 @@ hg clone https://hg@bitbucket.org/akiko_pusu/redmine_issue_templates
 # removed simplecov from plugin
 
 
-cd $redmine_htdocs_dir dir
-rake redmine:plugins:migrate --trace
+cd $redmine_htdocs_dir 
+bin/rake redmine:plugins:migrate RAILS_ENV=production
 require_restart=true
 
 
